ea1020ceac76f5f0319d117accf122b828c9665a
Sebastien VIGUIER <sebastien.viguier@etu.parisdescartes.fr>
2016-12-02 00:21:57 +0100

    shit has been done

--- Communigee/src/SiteBundle/Controller/AppController.php
+++ Communigee/src/SiteBundle/Controller/AppController.php
@@ -8,11 +8,14 @@ class AppController extends Controller
 {
     public function hygieneAction()
     {
-        return $this->render('SiteBundle:Site:hygiene.html.twig');
+        return $this->render('SiteBundle:vues:hygiene.html.twig');
     }
 	
 	public function santeAction(){
 		return $this->render('SiteBundle:vues:sante.html.twig');
 	}
 
+	public function communicationAction(){
+		return $this->render('SiteBundle:vues:communication.html.twig');
+	}
 }
